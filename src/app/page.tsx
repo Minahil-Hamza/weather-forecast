721ad65ec3d3fa583bc2a15a56f9faa2829fec23
dr.minahil16@gmail.com <dr.minahil16@gmail.com>
2024-10-03 23:01:42 +0500

    project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import WeatherWidget from "@/components/ui/weather_widget";
+
+export default function Home() {
+    return (
+        <div>
+            <WeatherWidget />
+        </div>
+    )
+
+}
